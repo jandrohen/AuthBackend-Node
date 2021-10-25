652314c57970e584cfe8cac9698bdfbc94bc22cf
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-25 18:34:43 +0200

    config server express (lesson 362 course angular)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('hola de nuevo');
+const express = require('express');
+
+
+//  Crear el servidor/aplicación de express
+const app = express();
+
+
+// GET
+app.get('/', (req, res) => {
+
+    res.json({
+        ok: true,
+        msg: "Todo salio bien",
+        uid: 1234
+    });
+
+});
+
+
+
+
+app.listen(4000, () =>{
+    console.log(`servidor corriendo en puerto ${ 4000 }`);
+})
