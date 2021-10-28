f1d62b1fdb2f1aaccd010b992136957829a722c5
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-10-28 15:43:53 +0200

    Create User in database with mongoose (lesson 374 course angular)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,49 @@
-const { response } = require('express')
+const { response, json} = require('express');
+const Usuario = require('../models/Usuario');
 
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
     const { name, email, password } = req.body;
 
+    try {
+        // Verificar el email
+        const usuario = await Usuario.findOne({email})
+
+        if (usuario){
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        // Crear usuario con el modelo
+        const dbUser = new Usuario(req.body);
+
+
+        // Hashear la contraseña
+
+
+        // Generar el JWT
+
+
+        // Crear usuario de BD
+        await dbUser.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+        });
+
+
+    } catch (e) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
     return res.json({
         ok: true,
         msg: 'Crear usuario /new'
