026dc4320684a6a3fd1fce134415b5ad25106638
Alejandro Arguello <alejandro.arguello@bosonit.com>
2021-11-03 17:58:55 +0100

    deploy angular application in node (course angular lesson 403)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const {dbConnection} = require("./db/config");
 require('dotenv').config();
 
@@ -19,6 +20,11 @@ app.use( express.json() );
 
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
+
+//Manejar otras rutas
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+})
 
 app.listen(process.env.PORT, () =>{
     console.log(`servidor corriendo en puerto ${ process.env.PORT }`);
